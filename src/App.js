9021377711f58a488d209be1ce232afc0c552d2c
elarhadu <elarhadu@gmail.com>
2023-01-19 11:30:26 +0000

    Install react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React, { Component } from 'react';
-import Calculator from './components/Calculator';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Calculator from './components/Calculator';
+import Home from './Pages/Home';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
 
-  render() {
-    return (
-      <>
-        <Calculator />
-      </>
-    );
-  }
+//   render() {
+//     return (
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Home />} exact />
+//           {/* <Calculator /> */}
+//         </Routes>
+//       </Router>
+//     );
+//   }
+// }
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        {/* <Calculator /> */}
+      </Routes>
+    </Router>
+  );
 }
