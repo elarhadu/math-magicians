0503be5cf715b5058903989cac245e4c96a7c66b
elarhadu <elarhadu@gmail.com>
2023-01-19 11:29:30 +0000

    Create pages for routing

---
+++ src/Pages/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote">
+    <p>
+      &#34;Pure mathematics is, in its way, the poetry of logical ideas.&#34; — Albert
+      Einstein, German theoretical physicist
+    </p>
+  </div>
+);
+export default Quote;
