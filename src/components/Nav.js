7df1d4bee3e5eed011a698972fd91c865b694e65
elarhadu <elarhadu@gmail.com>
2023-01-25 16:27:03 +0000

    Use Link instead of anchor tags for nav menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.css';
 
 export default function Nav() {
@@ -8,13 +9,13 @@ export default function Nav() {
       <nav>
         <ul>
           <li>
-            <a href="/" className="nav-link">Home</a>
+            <Link to="/" className="nav-link">Home</Link>
           </li>
           <li>
-            <a href="/calculator" className="nav-link">Calculator</a>
+            <Link to="/calculator" className="nav-link">Calculator</Link>
           </li>
           <li>
-            <a href="/quote" className="nav-link">Quote</a>
+            <Link to="/quote" className="nav-link">Quote</Link>
           </li>
         </ul>
       </nav>
